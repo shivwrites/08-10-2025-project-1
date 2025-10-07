f61cd93857d3ebf371edfd0c03057c908b642c4a
shivwrites <ciaoshiv@gmail.com>
2025-10-08 01:32:24 +0530

    Start repository

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+      <iframe
+        src="/landing.html"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+          display: 'block'
+        }}
+        title="Career Clarified Landing Page"
+      />
+    </div>
+  );
+}
+
+export default App;
